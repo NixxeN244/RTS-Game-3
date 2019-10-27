62303efc44a44ef84c3d2a102a8887117ab56e23
NixxeN244 <ns233282@gmail.com>
2019-10-27 16:59:17 +0200

    generating the uints
    making the units spawn

---
+++ RTS Game atempt 3/Assets/scripts/MELEEUNIT.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MELEEUNIT : MonoBehaviour
+{
+    GameObject meleeUnit;
+
+    private float health;
+    private float speed;
+    private float attackDam;
+    private int a = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ RTS Game atempt 3/Assets/scripts/Spawnnig_of_Units.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnnig_of_Units : MonoBehaviour
+{
+    public GameObject meleeUnits;
+    public GameObject rangedUnits;
+    public GameObject wizardUnits;
+    public GameObject base1;
+    public GameObject base2;
+    public GameObject base3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            //GameObject spawnW = Instantiate(wizardUnits, base1.transform.position, Quaternion.identity);
+            
+           GameObject spawnM = Instantiate(meleeUnits, base1.transform.position, Quaternion.identity);
+            spawnM.GetComponent<Rigidbody>().AddForce(new Vector3(0f, 0f, 5f), ForceMode.Impulse);
+            //GameObject spawnR = Instantiate(rangedUnits, base1.transform.position, Quaternion.identity);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
